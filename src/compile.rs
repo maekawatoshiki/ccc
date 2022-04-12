0c189f502ab3f280f52a0ce27d8bd196f12990fa
maekawatoshiki <konndennsa@gmail.com>
2022-04-13 00:56:31 +0900

    Add error enum

--- src/compile.rs
+++ src/compile.rs
@@ -1,7 +1,8 @@
 use lang_c::{ast::ExternalDeclaration, driver::Parse, span::Node};
 use log;
+use thiserror::Error as ThisError;
 
-pub fn compile(parsed: Parse) -> Result<(), ()> {
+pub fn compile(parsed: Parse) -> Result<(), Error> {
     let mut ctx = Context::default();
     compile_translation_unit(&mut ctx, parsed.unit.0)
 }
@@ -10,10 +11,13 @@ pub fn compile(parsed: Parse) -> Result<(), ()> {
 #[derive(Default)]
 struct Context {}
 
+#[derive(Debug, ThisError)]
+pub enum Error {}
+
 fn compile_translation_unit(
     ctx: &mut Context,
     decls: Vec<Node<ExternalDeclaration>>,
-) -> Result<(), ()> {
+) -> Result<(), Error> {
     log::info!("Skip compilation process since it's unimplemented");
     Ok(())
 }
