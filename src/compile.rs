de3e8c09215a5e76ab4564c5192ff8d3103f94a9
maekawatoshiki <konndennsa@gmail.com>
2022-04-13 00:23:12 +0900

    Add a driver function 'compile()'

---
+++ src/compile.rs
@@ -0,0 +1,5 @@
+use lang_c::driver::Parse;
+
+pub fn compile(parsed: Parse) -> Result<(), ()> {
+    todo!()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod compile;
+
+use compile::compile;
+use lang_c::driver::{parse, Config};
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::default();
+    let result = parse(&config, "example.c").expect("Error occurred while parsing");
+    compile(result).expect("Error occurred while compiling")
 }
