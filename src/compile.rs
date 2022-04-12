2064585dd6df084a3a028332c4810cebd6067b8d
maekawatoshiki <konndennsa@gmail.com>
2022-04-13 00:52:55 +0900

    Add context for compile

--- src/compile.rs
+++ src/compile.rs
@@ -1,5 +1,19 @@
-use lang_c::driver::Parse;
+use lang_c::{ast::ExternalDeclaration, driver::Parse, span::Node};
+use log;
 
-pub fn compile(_parsed: Parse) -> Result<(), ()> {
-    todo!()
+pub fn compile(parsed: Parse) -> Result<(), ()> {
+    let mut ctx = Context::default();
+    compile_translation_unit(&mut ctx, parsed.unit.0)
+}
+
+/// A context for whole compilation process.
+#[derive(Default)]
+struct Context {}
+
+fn compile_translation_unit(
+    ctx: &mut Context,
+    decls: Vec<Node<ExternalDeclaration>>,
+) -> Result<(), ()> {
+    log::info!("Skip compilation process since it's unimplemented");
+    Ok(())
 }
